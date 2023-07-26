037263c81256abeb57d9a84370dabc58dbc1323d
Sh_Haydarova <s.haydarzada@gmail.com>
2023-07-26 15:54:38 +0400

    added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+let uniquechar = "";
+let uniquechar2 = ""
 function generate() {
     captcha = document.getElementById("captcha");
-    let uniquechar = "";
-    let uniquechar2 = ""
 
     const randomchar =
         "0123456789";
@@ -16,13 +16,36 @@ function generate() {
 
 }
 
+let payment = document.querySelectorAll("#payment h4");
 let inputs = document.querySelectorAll("#input div");
+let flats = document.querySelectorAll("#flats_choose h4")
 
-let numbers = document.querySelectorAll("#numbers div")
+let numbers = document.querySelectorAll("#numbers div");
+let j = 0;
 
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener("click", () => {
-        inputs[0].innerHTML=numbers[i].innerHTML
+        if (j < 6) {
+            if (!inputs[j].innerHTML)
+                inputs[j].innerHTML = numbers[i].innerHTML;
+
+            uniquechar2 += numbers[i].innerHTML;
+            if (j == 5 && uniquechar != uniquechar2) {
+                alert("Captcha kodu yanlisdir")
+            }
+            j++;
+        }
+    })
+}
+
+for (let i = 0; i < payment.length; i++) {
+    payment[i].addEventListener("click", () => {
+        payment[i].classList.toggle("selected")
+    })
+}
+for (let i = 0; i < flats.length; i++) {
+    flats[i].addEventListener("click", () => {
+        flats[i].classList.toggle("selected")
     })
 }
 
